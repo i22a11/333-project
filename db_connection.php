6fcd25521eb304a63201065bd235044141a3bc49
Abdulrahman Idrees <akhaledlarus@gmail.com>
2024-12-09 17:10:14 +0300

    env for deployment

--- db_connection.php
+++ db_connection.php
@@ -7,9 +7,9 @@ ini_set('display_startup_errors', 1);
 function db_connect(): PDO
 {
     $host = 'localhost';
-    $db_name = 'itcs333';
-    $db_user = 'root';
-    $db_password = '';
+    $db_name = getenv('DB_NAME') ?: 'itcs333';
+    $db_user = getenv('DB_USER') ?: 'root';
+    $db_password = getenv('DB_PASSWORD') ?: '';
     $db = "mysql:host=$host;dbname=$db_name;charset=utf8mb4";
     $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
     return new PDO($db, $db_user, $db_password, $options);
